a582f6339ec3394de8e3225bd826965823b0972a
Nayana-R <plant56048@gmail.com>
2025-12-15 05:29:23 +0530

    Create python dice.py
    Add dice program
    
    Implements a simple dice roll, 2 dice will roll, using Python's random module.
    Prints a number between 1 and 6 for basic practice.

---
+++ python dice.py
@@ -0,0 +1,12 @@
+import rabdom
+
+while True:
+  choice=input('Roll the dice(y/n):')
+               if choice==y:
+               die1=random.randit(1,6)
+               die1=random.randit(1,6)
+          elif choice==n:
+               print('Thanks for playing')
+            break
+         else:
+              pront('invalid choice')
