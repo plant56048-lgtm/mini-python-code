6f64a37482bfbb090f0e40c2e9c8ce4469cb80ef
Nayana-R <plant56048@gmail.com>
2025-12-30 11:13:36 +0530

    Update python dice.py
    Corrected few code lines

--- python dice.py
+++ python dice.py
@@ -1,12 +1,13 @@
-import rabdom
+import random
 
 while True:
-  choice=input('Roll the dice(y/n):')
-               if choice==y:
-               die1=random.randit(1,6)
-               die1=random.randit(1,6)
-          elif choice==n:
-               print('Thanks for playing')
-            break
-         else:
-              pront('invalid choice')
+    choice = input('Roll the dice (y/n): ')
+    if choice == 'y':
+        die1 = random.randint(1, 6)
+        die2 = random.randint(1, 6)
+        print(f'You rolled {die1} and {die2}')
+    elif choice == 'n':
+        print('Thanks for playing!')
+        break
+    else:
+        print('Invalid choice, please enter y or n.')
