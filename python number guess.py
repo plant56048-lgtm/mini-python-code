63d014675f38fbe33508e390fc1598262c3bdcf6
Nayana-R <plant56048@gmail.com>
2026-01-03 14:58:46 +0530

    Create python number guess.py
    - Challenge the computer to Rock-Paper-Scissors in Python 🎉
    - Classic Rock-Paper-Scissors game with emoji-powered fun

---
+++ python number guess.py
@@ -0,0 +1,30 @@
+import random
+
+emojis = {'r': '🪨', 'p': '📃', 's': '✂️'}
+choices = ('r', 'p', 's')
+
+while True:
+    user_choice = input('Rock, paper, scissor? (r,p,s): ').lower()
+    if user_choice not in choices:
+        print('Invalid choice ❌')
+        continue
+
+    computer_choice = random.choice(choices)
+
+    print(f'You chose {emojis[user_choice]}')
+    print(f'Computer chose {emojis[computer_choice]}')
+
+    if user_choice == computer_choice:
+        print('It is a tie 🤝')
+    elif (
+        (user_choice == 'r' and computer_choice == 's') or
+        (user_choice == 's' and computer_choice == 'p') or
+        (user_choice == 'p' and computer_choice == 'r')
+    ):
+        print('You win 🎉')
+    else:
+        print('You lose 😢')
+
+    should_continue = input('Continue? (y/n): ').lower()
+    if should_continue == 'n':
+        break
